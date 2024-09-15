29c7a2718e9206ae3e8f7cc5d3860abe0e8f4753
veprogames <75524847+veprogames@users.noreply.github.com>
2024-09-15 19:31:40 +0200

    add enemy spawner

---
+++ spawner/spawner.gd
@@ -0,0 +1,12 @@
+class_name Spawner
+extends Node2D
+
+var EnemyScene: PackedScene = preload("res://enemy/enemy.tscn")
+
+func _on_timer_timeout() -> void:
+	var enemy: Enemy = EnemyScene.instantiate() as Enemy
+	enemy.position = Vector2(
+		randf_range(-50.0, 50.0),
+		randf_range(-50.0, 50.0)
+	) + global_position
+	owner.add_child(enemy)
