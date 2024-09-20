15f260ea5349bfbd6cb02519a5bef12783c882b7
veprogames <75524847+veprogames@users.noreply.github.com>
2024-09-20 12:25:49 +0200

    add level spawner that spawns enemies at random positions

--- enemy/enemy_factory.gd
+++ enemy/enemy_factory.gd
@@ -8,6 +8,13 @@ static var Enemies: Array[PackedScene] = [
 	preload("res://enemy/enemies/enemy_green.tscn"),
 ]
 
+
+static func get_random_tier(level_time: float) -> int:
+	var bias: float = clampf(-log(randf()) / log(5), 0, 3)
+	var tier: int = int(level_time / 120 + bias) - 1
+	return clampi(tier, 0, Enemies.size() - 1)
+
+
 static func create_random(tier: int) -> Enemy:
 	assert(tier >= 0 and tier < Enemies.size())
 	
---
+++ spawner/level_spawner.gd
@@ -0,0 +1,16 @@
+class_name LevelSpawner
+extends Node
+
+@onready var level: Level = get_tree().current_scene as Level
+@onready var player: Player = get_tree().get_first_node_in_group(&"player") as Player
+
+
+func _ready() -> void:
+	assert(is_instance_valid(level) and is_instance_valid(player))
+
+
+func _on_timer_timeout() -> void:
+	var tier: int = EnemyFactory.get_random_tier(level.time)
+	var enemy: Enemy = EnemyFactory.create_random(tier)
+	enemy.position = player.position + 500 * Vector2.RIGHT.rotated(randf() * TAU)
+	level.add_child(enemy)
