24ddbef0ba60c5c85a0a0f7e0c9b56be54408821
veprogames <75524847+veprogames@users.noreply.github.com>
2024-09-21 13:24:08 +0200

    add more enemies (purple, magenta, white, black)

--- enemy/enemy_factory.gd
+++ enemy/enemy_factory.gd
@@ -8,12 +8,16 @@ static var Enemies: Array[PackedScene] = [
 	preload("res://enemy/enemies/enemy_green.tscn"),
 	preload("res://enemy/enemies/enemy_cyan.tscn"),
 	preload("res://enemy/enemies/enemy_blue.tscn"),
+	preload("res://enemy/enemies/enemy_purple.tscn"),
+	preload("res://enemy/enemies/enemy_magenta.tscn"),
+	preload("res://enemy/enemies/enemy_white.tscn"),
+	preload("res://enemy/enemies/enemy_black.tscn"),
 ]
 
 
 static func get_random_tier(level_time: float) -> int:
 	var bias: float = clampf(-log(randf()) / log(5), 0, 3)
-	var tier: int = int(level_time / 120 + bias - 0.5)
+	var tier: int = int(level_time / 180 + bias - 0.5)
 	return clampi(tier, 0, Enemies.size() - 1)
 
 
